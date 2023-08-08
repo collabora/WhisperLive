c6572089bfdc9d4d3e8e734ab9034d8c371d361d
makaveli10 <suryanvineet47@gmail.com>
2023-08-08 21:12:39 +0800

    increase max clients to 4

--- whisper_live/server.py
+++ whisper_live/server.py
@@ -38,7 +38,7 @@ class TranscriptionServer:
         self.clients = {}
         self.websockets = {}
         self.clients_start_time = {}
-        self.max_clients = 1
+        self.max_clients = 4
         self.max_connection_time = 600 # in seconds
     
     def get_wait_time(self):
