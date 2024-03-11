5ca8fefac75d125df6a16249332d925178c8c28d
makaveli10 <vineet.suryan@collabora.com>
2024-03-11 06:00:05 -0400

    option to run faster_whisper backend

--- docker/scripts/run-whisperlive.sh
+++ docker/scripts/run-whisperlive.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 echo "MODEL_SIZE is set to: $MODEL_SIZE"
+echo "BACKEND is set to: $BACKEND"
 
 test -f /etc/shinit_v2 && source /etc/shinit_v2
 
@@ -12,13 +13,16 @@ whisper_model_trt="whisper_${MODEL_SIZE//./_}"
 echo "$whisper_model_trt"
 
 cd /root/WhisperLive
-if [[ $MODEL_SIZE == *".en" ]]; then
-    # If MODEL_SIZE ends with '.en', run without -m option
-    exec python3 run_server.py -p 9090 -b tensorrt \
-        -trt /root/scratch-space/models/"$whisper_model_trt"
+
+if [ "$BACKEND" == "tensorrt" ]; then
+    if [[ $MODEL_SIZE == *".en" ]]; then
+        exec python3 run_server.py -p 9090 -b $BACKEND \
+            -trt /root/scratch-space/models/"$whisper_model_trt"
+    else
+        exec python3 run_server.py -p 9090 -b $BACKEND \
+            -trt /root/scratch-space/models/"$whisper_model_trt" \
+            -m
+    fi
 else
-    # If MODEL_SIZE does not end with '.en', run with -m option
-    exec python3 run_server.py -p 9090 -b tensorrt \
-        -trt /root/scratch-space/models/"$whisper_model_trt" \
-        -m
+    exec python3 run_server.py
 fi
