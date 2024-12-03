49e232bc4d771dd0cf7cd67f1f9a67848c8c7439
makaveli10 <vineet.suryan@collabora.com>
2024-12-03 20:09:41 +0530

    Set segment.completed to False by default
    Signed-off-by: makaveli10 <vineet.suryan@collabora.com>

--- whisper_live/client.py
+++ whisper_live/client.py
@@ -112,9 +112,9 @@ class Client:
         for i, seg in enumerate(segments):
             if not text or text[-1] != seg["text"]:
                 text.append(seg["text"])
-                if i == len(segments) - 1 and not seg["completed"]:
+                if i == len(segments) - 1 and not seg.get("completed", False):
                     self.last_segment = seg
-                elif (self.server_backend == "faster_whisper" and seg["completed"] and
+                elif (self.server_backend == "faster_whisper" and seg.get("completed", False) and
                       (not self.transcript or
                         float(seg['start']) >= float(self.transcript[-1]['end']))):
                     self.transcript.append(seg)
