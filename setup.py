4e9eb3f443222a9ad13010c5e77c7ce0baa758bc
meezlung <gabrielmislang101@gmail.com>
2025-04-15 20:28:49 +0800

    fix: pin numpy dependency to <2.0 to avoid errors

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
         "soundfile",
         "tokenizers==0.20.3",
         "librosa",
-        "numpy==1.26.4",
+        "numpy<2.0",
         "openvino",
         "openvino-genai",
         "openvino-tokenizers",
